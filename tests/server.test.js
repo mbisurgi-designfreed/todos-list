6d3cb9bc9a15df434c79218b0edd08f624047062
mbisurgi <maximiliano@designfreed.com>
2018-01-13 02:14:14 -0300

    Add test cases for GET /todos/:id

--- tests/server.test.js
+++ tests/server.test.js
@@ -1,12 +1,15 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const app = require('../server/server');
 const Todo = require('../server/models/todo.model');
 
 const todos = [{
+    _id: new ObjectID(),
     text: 'First test todo'
 }, {
+    _id: new ObjectID(),
     text: 'Second test todo'
 }];
 
@@ -16,6 +19,50 @@ beforeEach((done) => {
             return Todo.insertMany(todos);
         })
         .then(() => done());
+});
+
+describe('GET /todos/:id', () => {
+    it('should return a todo', (done) => {
+        const id = todos[0]._id.toHexString();
+
+        request(app)
+            .get(`/todos/${id}`)
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.text).toBe(todos[0].text);
+            })
+            .end(done);
+    });
+
+    it('should return 404 if todo not found', (done) => {
+        const id = new ObjectID().toHexString();
+
+        request(app)
+        .get(`/todos/${id}`)
+        .expect(404)
+        .end(done);
+    });
+
+    it('should return 404 if id is not valid', (done) => {
+        const id = '5a598b22fd0a96e876cadb921';
+
+        request(app)
+        .get(`/todos/${id}`)
+        .expect(404)
+        .end(done);
+    });
+});
+
+describe('GET /todos', () => {
+    it('should get all todos', (done) => {
+        request(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.length).toBe(2);
+            })
+            .end(done);
+    });
 });
 
 describe('POST /todos', () => {
@@ -68,14 +115,3 @@ describe('POST /todos', () => {
     });
 });
 
-describe('GET /todos', () => {
-    it('should get all todos', (done) => {
-        request(app)
-            .get('/todos')
-            .expect(200)
-            .expect((res) => {
-                expect(res.body.length).toBe(2);
-            })
-            .end(done);
-    });
-});
