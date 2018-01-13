da7b3ba939e19d1ac98e81748df73015ccf416cb
mbisurgi <maximiliano@designfreed.com>
2018-01-13 03:13:56 -0300

    Add PATCH /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const parser = require('body-parser');
+const _ = require('lodash');
 const { ObjectID } = require('mongodb');
 
 const mongoose = require('./db/mongoose');
@@ -52,6 +53,34 @@ app.post('/todos', (req, res) => {
         })
         .catch((err) => {
             res.status(400).send(err);
+        });
+});
+
+app.patch('/todos/:id', (req, res) => {
+    const id = req.params.id;
+    const todo = _.pick(req.body, ['text', 'completed']);
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    if (_.isBoolean(todo.completed) && todo.completed) {
+        todo.completedAt = new Date().getTime();
+    } else {
+        todo.completed = false;
+        todo.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, { $set: todo }, { new: true })
+        .then((todo) => {
+            if (!todo) {
+                return res.status(404).send();
+            }
+
+            res.status(200).send(todo);
+        })
+        .catch((err) => {
+            res.status(400).send();
         });
 });
 
