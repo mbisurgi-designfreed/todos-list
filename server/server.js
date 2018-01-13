97c9921681cfb59427aa29593bab7042176de1dc
mbisurgi <maximiliano@designfreed.com>
2018-01-12 21:06:27 -0300

    Add GET/todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const parser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 const mongoose = require('./db/mongoose');
 const Todo = require('./models/todo.model');
@@ -16,6 +17,26 @@ app.get('/todos', (req, res) => {
         })
         .catch((err) => {
             res.status(400).send(err);
+        });
+});
+
+app.get('/todos/:id', (req, res) => {
+    const id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findById(id)
+        .then((todo) => {
+            if (!todo) {
+                return res.status(404).send();
+            }
+
+            res.status(200).send(todo);
+        })
+        .catch((err) => {
+            res.status(400).send();
         });
 });
 
