f8e96fbd5ca570606733e79bd5af55bfcd10845a
mbisurgi <maximiliano@designfreed.com>
2018-01-22 22:09:10 -0300

    Add tests for POST /users/login

--- tests/server.test.js
+++ tests/server.test.js
@@ -4,6 +4,7 @@ const { ObjectID } = require('mongodb');
 
 const app = require('../server/server');
 const Todo = require('../server/models/todo.model');
+const User = require('../server/models/user.model');
 const { todos, populateTodos, users, populateUsers } = require('./seed/seed');
 
 beforeEach(populateUsers);
@@ -226,7 +227,7 @@ describe('POST /users', () => {
 
         request(app)
             .post('/users')
-            .send({email, password})
+            .send({ email, password })
             .expect(400)
             .expect((res) => {
                 expect(res.body.errors).toExist();
@@ -240,12 +241,67 @@ describe('POST /users', () => {
 
         request(app)
             .post('/users')
-            .send({email, password})
+            .send({ email, password })
             .expect(400)
             .expect((res) => {
                 expect(res.body.errmsg).toExist();
             })
             .end(done);
+    });
+});
+
+describe('POST /users/login', () => {
+    it('should login user and return auth token', (done) => {
+        request(app)
+            .post('/users/login')
+            .send({ email: users[1].email, password: users[1].password })
+            .expect(200)
+            .expect((res) => {
+                expect(res.headers['x-auth']).toExist();
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                User.findById(users[1]._id)
+                    .then((user) => {
+                        expect(user.tokens[0]).toInclude({
+                            access: 'auth',
+                            token: res.headers['x-auth']
+                        });
+
+                        done()
+                    })
+                    .catch((err) => {
+                        done(err);
+                    })
+            });
+    });
+
+    it('should reject invalid login', (done) => {
+        request(app)
+            .post('/users/login')
+            .send({ email: users[1].email, password: 'maximati12' })
+            .expect(400)
+            .expect((res) => {
+                expect(res.headers['x-auth']).toNotExist();
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                User.findById(users[1]._id)
+                    .then((user) => {
+                        expect(user.tokens.length).toBe(0);
+
+                        done()
+                    })
+                    .catch((err) => {
+                        done(err);
+                    })
+            });
     });
 });
 
